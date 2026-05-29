188729fe3a2bf8175cf11c732afbca3b33808f54
Abdullahjaved-82 <abdullahjavec@gmail.com>
2026-05-30 01:15:53 +0500

    Configure dynamic serverless storage override to /tmp on Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -18,3 +18,23 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Dynamic storage path override for serverless hosting on Vercel
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL_URL']) || isset($_SERVER['VERCEL_URL'])) {
+    $app->useStoragePath('/tmp');
+
+    $storageDirs = [
+        '/tmp/framework/views',
+        '/tmp/framework/cache',
+        '/tmp/framework/sessions',
+        '/tmp/logs',
+    ];
+
+    foreach ($storageDirs as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+    }
+}
+
+return $app;
