b012b96ef534ca7314b36dfb4790eec90c1695a1
Abdullahjaved-82 <abdullahjavec@gmail.com>
2026-05-30 16:17:33 +0500

    Redirect root URL to login

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\OnboardingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::middleware('auth')->group(function () {
