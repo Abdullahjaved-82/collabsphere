b14d784f3beadfa93724f21fa847a5d12aaa32c0
Abdullahjaved-82 <abdullahjavec@gmail.com>
2026-06-13 18:33:31 +0500

    fix: disable named prepared statements for pgsql to support neon pgbouncer transaction pooling without breaking boolean casts

--- config/database.php
+++ config/database.php
@@ -97,6 +97,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => extension_loaded('pdo_pgsql') ? [
+                \PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+            ] : [],
         ],
 
         'sqlsrv' => [
